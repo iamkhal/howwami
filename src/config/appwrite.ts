48e74be577da49a6960746a46503bb9542b9a65e
iamkhal <consigliere1604@gmail.com>
2024-12-06 12:07:34 +0100

    second commit

--- src/config/appwrite.ts
+++ src/config/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('675269230027388cbbfc');
+    .setProject('6752d0de00147406bced');
 
 export const account = new Account(client);
-export const databases = new Databases(client, '67526a34000e0cf96404');
+export const databases = new Databases(client, '6752d675000a004a8506');
